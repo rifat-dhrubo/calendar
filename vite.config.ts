b6f414a9b6609aee50c42c283119d4e4ce8ac8cd
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2022-08-12 12:33:51 +0600

    chore: added readme

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3001,
+  },
 });
